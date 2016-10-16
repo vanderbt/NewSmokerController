a0a9b81ee2359327ecd3a37e3080999a6f72424d
root <root@DeveloperPC.home.net>
2016-10-16 09:46:37 -0700

    Got the right index.php file into the repository.

--- WebFiles/index.php
+++ WebFiles/index.php
@@ -1,44 +1,37 @@
-<?php
-
-	include("connect.php"); 	
-	
-	$link=Connection();
-
-	$result=mysql_query("SELECT * FROM `environment` ORDER BY `ID` DESC LIMIT 1",$link);
-?>
-
 <html>
-   <head>
-      <title>Sensor Data</title>
-   </head>
-<body>
-   <h1>Temperature / Humidity Sensor Readings</h1>
-
-   <table border="1" cellspacing="1" cellpadding="1">
-		<tr>
-			<td>&nbsp;timeStamp&nbsp;</td>	
-			<td>&nbsp;airTemp&nbsp;</td>
-			<td>&nbsp;airHum&nbsp;</td>
-			<td>&nbsp;h20Temp&nbsp;</td>
-		</tr>
-
-      <?php 
-		  if($result!==FALSE){
-		     while($row = mysql_fetch_array($result)) {
-		        printf("<tr><td> &nbsp;%s </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp; </td><td> &nbsp;%s&nbsp <td></tr>", 
-		           $row["timeStamp"], $row["airTemp"], $row["airHum"], $row["h20Temp"]);
-		     }
-		     mysql_free_result($result);
-		     mysql_close();
-		  }
-      ?>
-
-   </table>
-	<img src="/rrdtool/tempGraph.png" alt="My Graph">
- 	<img src="/rrdtool/tempGraph2.png" alt="My Graph">
- 	<img src="/rrdtool/tempGraph3.png" alt="My Graph">
-
- 	<img src="/rrdtool/tempGraph4.png" alt="My Graph">
+<head>
+<meta http-equiv="refresh" content="20"> <!-- Refresh every 20 seconds -->
+<h1>
+	<title>Sensor Data</title>
+</head>
+<body
+ style="background-image: url(/smokerYun/images/bbqfireImag2.jpg);">
+<div id="image-container"> </div>
+<h1 style="color: red;"><span
+style="font-family: Helvetica,Arial,sans-serif;">Smoker
+ Data</span><br>
+</h1>
+<img src="/smokerYun/rrdtool/tempGraphSI2Hours.png" alt="My Graph"> <img
+<img src="/smokerYun/rrdtool/tempGraphSI4Hours.png" alt="My Graph"> <img
+<img src="/smokerYun/rrdtool/tempGraphSI8Hours.png" alt="My Graph"> <img
+<img src="/smokerYun/rrdtool/tempGraphSI.png" alt="My Graph"> <img
+</tr>
+</table>
 </body>
 </html>
 
+<form action="insert.php" method="post">
+
+<body
+<h1 style="color: red;"><span
+style="font-family: Helvetica, Arial, sans-erif;"><font color="red" size="5"><b>Smoker Setpoint:</b></font></span><br> <input type="text" name="setPoint"><br><br>
+</h1>
+<input type="submit" />
+
+</form>
+</tbody>
+</table>
+
+
+</body>
+</html>
